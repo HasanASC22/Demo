b515b2f277a0df98dba8d2f67db6d7e04ec83d60
Hasan Islam <hislam@stu.allstarcode.org>
2022-08-18 11:06:28 -0400

    almost ready for demo day updated some features commented out the api map since there is a time restriction on everytime we refresh

--- CrimeMap/crimeometer-data-visualization-kit/config/config.js
+++ CrimeMap/crimeometer-data-visualization-kit/config/config.js
@@ -1,8 +1,8 @@
-const x_api_key_level1 = '';
-/* With the API Key Level 1 you have access to:
+/*const x_api_key_level1 = 'u5MrZd7OFR2jvzzwgsPd36WYiShIediY9owvyIpi';
+ With the API Key Level 1 you have access to:
 - crimeometer_google_maps_heatmap
 - crimeometer_google_maps_markers
-*/
+
 var x_api_key = x_api_key_level1;
 
-const api_mapbox_accessToken = 'pk.eyJ1Ijoibmlra29zIiwiYSI6ImNsNndhaHEyNjI5Mm8zZG9kMXZ5aWd3NXMifQ.AstvVePNcQo-mv7D9JDtJw';
+const api_mapbox_accessToken = 'pk.eyJ1Ijoibmlra29zIiwiYSI6ImNsNndhaHEyNjI5Mm8zZG9kMXZ5aWd3NXMifQ.AstvVePNcQo-mv7D9JDtJw';*/
